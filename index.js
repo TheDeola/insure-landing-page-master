d1d9f2373bc5a8502cb9d9a1587be5b7a1e49554
TheDeola <aadetunji012@gmail.com>
2022-08-18 14:10:34 +0200

    hamburger toggle function

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const menuIcon = document.querySelector(".icon-menu");
+const menuIcon = document.querySelector(".mobile-menu-container");
 const menuContainer = document.querySelector(".menu-container");
 const viewButton = document.getElementById("view-button");
 
+const hamburger = document.getElementById("hamburger");
+
 const dialog = document.querySelector("dialog");
 
-menuIcon.addEventListener("click", () => {
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle('show');
   dialog.showModal();
-});
-
-viewButton.addEventListener("click", () => {
-  toggle.classList.add('.close-icon');
 });
 
 dialog.addEventListener("click", (event) => {
